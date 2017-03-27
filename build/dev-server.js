833dea1d27b2fa463f2422cd2f2eef60117dc823
lsling <952543325@qq.com>
2017-03-27 11:18:10 +0800

    initial configuration

--- build/dev-server.js
+++ build/dev-server.js
@@ -14,6 +14,39 @@ var port = process.env.PORT || config.dev.port
 var proxyTable = config.dev.proxyTable
 
 var app = express()
+
+// ==== start 模拟后台数据
+var appData = require('../data.json');
+var seller = appData.seller;
+var goods = appData.goods;
+var ratings = appData.ratings;
+
+var apiRouters = express.Router();
+
+apiRouters.get('/seller',function(req,res) {
+  res.json({
+    errno: 0,
+    data: seller
+  });
+})
+
+apiRouters.get('/goods',function(req,res) {
+  res.json({
+    errno: 0,
+    data: goods
+  });
+})
+
+apiRouters.get('/ratings',function(req,res) {
+  res.json({
+    errno: 0,
+    data: ratings
+  });
+})
+app.use('/api', apiRouters);
+// ==== end 模拟后台数据
+
+
 var compiler = webpack(webpackConfig)
 
 var devMiddleware = require('webpack-dev-middleware')(compiler, {
